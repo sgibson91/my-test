ff5c20ec7d2c4294e5ec0b2b8a0e5b05b18afb53
Sarah Gibson <44771837+sgibson91@users.noreply.github.com>
2023-11-29 15:55:41 +0000

    Update workflow_run.py

--- workflow_run.py
+++ workflow_run.py
@@ -1,10 +1,28 @@
 import os
 import json
 
-workflow_run = os.environ.get("WORKFLOW_RUN", None)
+import requests
 
+api_url = "https://api.github.com"
+
+# GITHUB_TOKEN = ${{ secrets.GITHUB_TOKEN }} in workflow file
+token = os.environ.get("GITHUB_TOKEN", None)
+if token is None:
+    raise ValueError("GITHUB_TOKEN must be set!")
+
+# WORKFLOW_RUN = ${{ github.event.workflow_run }} in workflow file
+workflow_run = os.environ.get("WORKFLOW_RUN", None)
 if workflow_run is None:
     raise ValueError("WORKFLOW_RUN must be set!")
-
 workflow_run = json.loads(workflow_run)
-print(workflow_run)
+
+# Set headers to send with requests
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {token}",
+}
+
+# List all artifacts for the workflow run
+resp = requests.get(workflow_run["artifacts_url"], headers=headers, params={"per_page": 100})
+all_artifacts = resp.json()["artifacts"]
+print(all_artifacts)
