1ba51127d7fa398b9367316fba43d14f0829b6d3
Sarah Gibson <44771837+sgibson91@users.noreply.github.com>
2023-11-29 16:03:17 +0000

    Update workflow_run.py

--- workflow_run.py
+++ workflow_run.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 
 import requests
@@ -16,6 +17,9 @@ if workflow_run is None:
     raise ValueError("WORKFLOW_RUN must be set!")
 workflow_run = json.loads(workflow_run)
 
+# Artifact name
+artifact_name = os.environ.get("ARTIFACT_NAME", "hello-world")
+
 # Set headers to send with requests
 headers = {
     "Accept": "application/vnd.github+json",
@@ -25,4 +29,25 @@ headers = {
 # List all artifacts for the workflow run
 resp = requests.get(workflow_run["artifacts_url"], headers=headers, params={"per_page": 100})
 all_artifacts = resp.json()["artifacts"]
-print(all_artifacts)
+
+# If "Link" is present in the response headers, that means that the results are
+# paginated and we need to loop through them to collect all the results.
+# It is unlikely that we will have more than 100 artifact results for a single
+# worflow ID however.
+while ("Link" in resp.headers.keys()) and ('rel="next"' in res.headers["Link"]
+):
+    next_url = re.search(r'(?<=<)([\S]*)(?=>; rel="next")', resp.headers["Link"])
+    resp = requests.get(next_url.group(0), headers=headers)
+    all_artifacts.extend(resp.json()["artifacts"])
+
+# Filter for the artifact with the name we want: 'hello-world'
+indx, artifact_id = next(
+    ((i, artifact["id"]) for i, artifact in enumerate(all_artifacts) if artifact["name"] == artifact_name),
+    (None, None),
+)
+
+if artifact_id is None:
+    print(f"No artifact found called '{artifact_name}' for workflow run: {run_id}")
+    sys.exit()
+
+print(all_artifacts[i])
